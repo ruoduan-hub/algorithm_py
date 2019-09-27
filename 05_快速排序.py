0e6c9029d6a6402005aad124a1218888adad60f2
chad97 <chad97@126.com>
2019-09-27 17:46:48 +0800

    qsort

--- 05_快速排序.py
+++ 05_快速排序.py
@@ -8,7 +8,7 @@ def quicksort(arr):
     if len(arr) < 2:
         return arr
     else:
-        pivot = arr[0 ]
+        pivot = arr[0]
 
         less = [i for i in arr[1:] if i <= pivot]
         greater = [i for i in arr[1:] if i > pivot]
